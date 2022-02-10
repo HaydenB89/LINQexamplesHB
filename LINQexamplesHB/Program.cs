08f38d4b25092054d47ce1ef9ca86ad7f503750a
hayden brown <haydenbrowncinci@gmail.com>
2022-02-10 13:47:18 -0500

    initial push

--- LINQexamplesHB/Program.cs
+++ LINQexamplesHB/Program.cs
@@ -3,7 +3,16 @@
 namespace LINQexamplesHB {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 23, 28, 225, 35, 500, 22, 15,
+                              -63, 7, 88, 53, -1, 12, 17 };
+            int total = 0;
+            foreach(int n in numbers) {
+                if (n % 2 == 1) {
+                    total += n;
+                    Console.WriteLine($"{n} ");
+                }
+            }
+            Console.WriteLine(total);
         }
     }
 }
