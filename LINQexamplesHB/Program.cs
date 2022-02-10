f463e0f10dfd4d8c2bb5306b7f08fb67cc3203d2
hayden brown <haydenbrowncinci@gmail.com>
2022-02-10 14:14:07 -0500

    commentd

--- LINQexamplesHB/Program.cs
+++ LINQexamplesHB/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Linq;  //dont foget to add this
 
 namespace LINQexamplesHB {
     class Program {
         static void Main(string[] args) {
             int[] numbers = { 23, 28, 225, 35, 500, 22, 15,
                               -63, 7, 88, 53, -1, 12, 17 };
+
+            int[] numLsThn200 = numbers
+                                .Where(t => t < 200).ToArray();
+
+            int[] numbers2 = numbers                             //Where is a method in LINQ, functions much like SQL
+                                .Where(n => n % 2 == 1)          //create the variable on the left sideof the lamda(=>) use it on the right
+                                .OrderByDescending(x => x)       // Again, much like SQL you can sort them with OrderBy
+                                .ToArray();                          
+            foreach (int n1 in numbers2) {                                                                      
+                Console.WriteLine($"odd: {n1}");                                                                                                    
+            }
+
             int total = 0;
             foreach(int n in numbers) {
                 if (n % 2 == 1) {
@@ -12,7 +25,7 @@ namespace LINQexamplesHB {
                     Console.WriteLine($"{n} ");
                 }
             }
-            Console.WriteLine(total);
+            Console.WriteLine($"Total is {total}");
         }
     }
 }
